032084acce94c9d0f12733e60a34184214469493
sherwy <tanabodee.l@outlook.com>
2017-07-30 14:55:30 +0700

    Update line controller (test)

--- src/main/java/com/tana/rest/BinaryToWordRest.java
+++ src/main/java/com/tana/rest/BinaryToWordRest.java
@@ -1,24 +1,21 @@
 package com.tana.rest;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.apache.log4j.Logger;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
-@RestController
+@LineMessageHandler
 public class BinaryToWordRest {
 	
 	private Logger LOGGER = Logger.getLogger(BinaryToWordRest.class);
 	
-	@RequestMapping(value="/testRest",method = RequestMethod.GET)
-	public ResponseEntity<String> getDetail() {
-		LOGGER.info("HERE WE ARE!");
-		return new ResponseEntity<>("Test REST",HttpStatus.OK);
-	}
+	@EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+		LOGGER.info("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+	
 }
